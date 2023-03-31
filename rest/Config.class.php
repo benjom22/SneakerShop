7a53b78d6b1c61b30f39d41f44a7e571acee3d33
lejlabrescic <lbrescic@gmail.com>
2023-04-01 00:41:49 +0200

    added rest

---
+++ rest/Config.class.php
@@ -0,0 +1,19 @@
+<?php
+class Config {
+    public static function DB_HOST(){
+        return 'localhost';
+    }
+
+    public static function DB_USERNAME(){
+        return 'root';
+    }
+
+    public static function DB_PASSWORD(){
+        return '';
+    }
+
+    public static function DB_SCHEMA(){
+        return 'sneakershop';
+    }
+}
+?>
---
+++ rest/index.php
@@ -0,0 +1,9 @@
+<?php
+require '../vendor/autoload.php';
+require './services/UserService.php';
+require_once './routes/UserRoutes.php';
+Flight::register('product_service','ProductService');
+Flight::register('user_service', 'UserService');
+Flight::json(Flight::user_service()->get_all());
+
+?>
