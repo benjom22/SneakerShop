9f3213d9fcf4d4a5b03927c0622f03a691ceb5b8
lejlabrescic <lbrescic@gmail.com>
2023-04-01 01:01:36 +0200

    rest updated

--- rest/index.php
+++ rest/index.php
@@ -4,6 +4,5 @@ require './services/UserService.php';
 require_once './routes/UserRoutes.php';
 Flight::register('product_service','ProductService');
 Flight::register('user_service', 'UserService');
-Flight::json(Flight::user_service()->get_all());
 
 ?>
